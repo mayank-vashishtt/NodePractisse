e5c9e32c4a4cb5ac3e4c4d91fcc2d82e80d1d5de
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-07-07 21:35:43 +0530

    rename and remove dir and make dir feature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');    // fs is a built-in module in Node.js
 
-const data = fs.readFileSync('file.txt');    // readFileSync is a synchronous method
+const data = fs.readFileSync('File.txt');    // readFileSync is a synchronous method
 console.log(data.toString());    // toString() is used to convert the buffer data to a string
 // without .toString it will give output in buffer format  or we can append an empty string in front of it 
 
@@ -8,6 +8,31 @@ console.log(data.toString());    // toString() is used to convert the buffer dat
 //     console.log('file modified successfully');
 // })    // writeFileSync is a synchronous method
 
-fs.appendFileSync('file.txt', "class is going on", 'utf-8', (err) =>{
-    console.log('file modified successfully');
-})    // appendFileSync is a synchronous method
+// fs.appendFileSync('file.txt', "class is going on", 'utf-8', (err) =>{
+//     console.log('file modified successfully');
+// })    // appendFileSync is a synchronous method
+
+// fs.mkdirSync('newFolder', (err) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('folder created successfully');
+
+// });    // mkdirSync is a synchronous method
+
+// fs.rmdir('newFolder', (err) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('folder deleted successfully');
+
+// }); 
+
+
+fs.renameSync('mmm.txt', 'File.txt', (err) => {
+    if (err) {
+        console.error('Error renaming file:', err);
+        return;
+    }
+    console.log('File renamed successfully');
+});
